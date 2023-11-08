630b47b98967b2be1cc2f78650b939f79411c5f3
musicDr1ven <gabe.kramer@gmail.com>
2023-11-07 20:30:54 -0500

    working

---
+++ src/views/home/index.tsx
@@ -0,0 +1,14 @@
+import React, {useEffect, useState} from 'react';
+
+
+export const Home = () => {
+    
+
+    return (
+        <div style={{ overflow: 'hidden', width:'100%', height: '100vh' }}>
+            Hello there.
+            
+
+        </div>
+    )
+};
